a6d6af39522ca7d8672982cbe461146fe2dafa0c
hanfuyin <18910490095@163.com>
2018-09-15 17:16:42 +0000

    User can upload topic image

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
 use App\Models\Topic;
+use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
-use App\Models\Category;
-use Auth;
+use App\Handlers\ImageUploadHandler;
 
 
 class TopicsController extends Controller
@@ -64,4 +65,25 @@ class TopicsController extends Controller
 
 		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
 	}
+
+	public function uploadImage(Request $request, ImageUploadHandler $uploader)
+    {
+        $data = [
+            'success' => false,
+            'msg' => '上传失败!',
+            'file_path' => ''
+        ];
+
+        if($file = $request->upload_file){
+            $result = $uploader->save($request->upload_file, 'topics', \Auth::id(), 1024);
+
+            if($result){
+                $data['file_path'] = $result['path'];
+                $data['msg'] = '上传成功!';
+                $data['success'] = true;
+            }
+        }
+
+        return $data;
+    }
 }
