efec49f775f1695ca2bde51de4f2302f19a4f83a
hanfuyin <18910490095@163.com>
2018-09-15 19:44:21 +0000

    Auto translate slug

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Topic;
+use App\Handlers\SlugTranslateHandler;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -21,7 +22,13 @@ class TopicObserver
 
     public function saving(Topic $topic)
     {
+        //过滤xss
         $topic->body = clean($topic->body, 'user_topic_body');
+        //生成话题摘录
         $topic->execrpt = make_excerpt($topic->body);
+        //如果slug字段无内容,使用翻译器对title进行翻译
+        if(!$topic->slug){
+            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,9 @@ Auth::routes();
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 //Route::get('/home', 'HomeController@index')->name('home');
 
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
+
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
