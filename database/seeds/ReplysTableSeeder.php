0f8b70db17efbd220808b587e5c2c42633debafe
hanfuyin <18910490095@163.com>
2018-09-16 16:21:47 +0000

    List user replies

---
+++ database/seeds/ReplysTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+use App\Models\Reply;
+use App\Models\User;
+use App\Models\Topic;
+
+class ReplysTableSeeder extends Seeder
+{
+    public function run()
+    {
+
+        DB::table('replies')->truncate();
+
+        $users  = User::all()->pluck('id')->toArray();
+        $faker = app(Faker\Generator::class);
+
+        DB::table('topics')->select('id')->orderBy('id', 'asc')->chunk(500, function ($topics) use ($users, $faker)
+        {
+            foreach ($topics as $topic)
+            {
+                $topic_id = $topic->id;
+                $replys = factory(Reply::class)->times(random_int(0, 100))->make()->each(function ($reply, $index) use ($users, $topic_id, $faker)
+                {
+                    $reply->user_id = $faker->randomElement($users);
+                    $reply->topic_id = $topic_id;
+                });
+                Reply::insert($replys->toArray());
+            }
+        });
+    }
+
+}
+
---
+++ resources/views/topics/_reply_box.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hanfuyin
+ * Date: 2018/9/16
+ * Time: 下午11:46
+ */
