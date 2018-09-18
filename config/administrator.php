0e2fff365eefbed4ab1e34596f687052f158962a
hanfuyin <18910490095@163.com>
2018-09-18 14:53:25 +0000

    Admin permission

--- config/administrator.php
+++ config/administrator.php
@@ -118,7 +118,7 @@ return array(
      *
      * @type string
      */
-    'login_path' => 'login',
+    'login_path' => 'permission-denied',
 
     /*
      * The logout path is the path where Administrator will send the user when they click the logout link
