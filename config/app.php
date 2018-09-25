eff82cef52b61ab326a0c7542bba6d0ecf8c868d
hanfuyin <18910490095@163.com>
2018-09-25 19:40:14 +0000

    add dingo

--- config/app.php
+++ config/app.php
@@ -177,6 +177,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Frozennode\Administrator\AdministratorServiceProvider::class,
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
     ],
 
