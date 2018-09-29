66125757292dc27fe93696fcae0d505b3b9bda08
hanfuyin <18910490095@163.com>
2018-09-29 20:22:57 +0000

    user update

--- routes/api.php
+++ routes/api.php
@@ -55,9 +55,15 @@ $api->version('v1', [
     ], function ($api){
 
         //需要 token 验证的接口
-        $api->group(['middleware' => 'api.auth'], function ($api){
+        $api->group([], function ($api){
             //当前登录用户信息
             $api->get('user', 'UsersController@me')->name('api.user.show');
+
+            //编辑登录用户信息
+            $api->patch('user', 'UsersController@update')->name('api.user.update');
+
+            //图片资源
+            $api->post('images', 'ImagesController@store')->name('api.images.store');
         });
     });
 
